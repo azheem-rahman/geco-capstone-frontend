e59dd611236bfa879e4e9dc9a6591e150dbf2f64
Azheem Rahman <azheem.rahman@gmail.com>
2023-09-21 23:56:03 +0800

    created layout for NP dashboard

---
+++ src/components/Dashboard/NPDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NPDashboard = () => {
+  return (
+    <div>
+      {/* see orders assigned to user > 2 categories -- New Orders and Existing Orders > new orders can accept/reject, existing orders can toggle completed*/}
+      {/* if got time, can sort existing orders based on assigned date */}
+    </div>
+  );
+};
+
+export default NPDashboard;
