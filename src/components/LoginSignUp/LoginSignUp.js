e94f2dbdd08c784c46aa2aececd9e89fe9125f69
Azheem Rahman <azheem.rahman@gmail.com>
2023-09-21 23:35:26 +0800

    edited sign up form with validation

--- src/components/LoginSignUp/LoginSignUp.js
+++ src/components/LoginSignUp/LoginSignUp.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import { Tab, Form, Button, Tabs } from "react-bootstrap";
+import {
+  Tab,
+  Form,
+  Button,
+  Tabs,
+  FloatingLabel,
+  InputGroup,
+  ButtonGroup,
+  ToggleButtonGroup,
+} from "react-bootstrap";
 
 const LoginSignUp = () => {
   // Login States
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
-  const [loginValid, setLoginValid] = useState("");
+  const [loginValid, setLoginValid] = useState(false);
   const [loginErrorMessages, setLoginErrorMessages] = useState({
     loginEmailError: "",
     loginPasswordError: "",
@@ -16,7 +25,7 @@ const LoginSignUp = () => {
   const [signUpLastName, setSignUpLastName] = useState("");
   const [signUpEmail, setSignUpEmail] = useState("");
   const [signUpPassword, setSignUpPassword] = useState("");
-  const [signUpValid, setSignUpValid] = useState("");
+  const [signUpValid, setSignUpValid] = useState(false);
   const [signUpErrorMessages, setSignUpErrorMessages] = useState({
     signUpFirstNameError: "",
     signUpLastNameError: "",
@@ -39,11 +48,123 @@ const LoginSignUp = () => {
   };
 
   // Sign Up Functions
-  const handleSignUpChange = (event) => {};
-  const validateSignUpName = (event) => {};
-  const validateSignUpEmail = (event) => {};
-  const validateSignUpPassword = (event) => {};
-  const handleSignUpSubmit = (event) => {};
+  const handleSignUpChange = (event) => {
+    if (
+      event.target.id === "signup-first-name" ||
+      event.target.id === "signup-last-name"
+    ) {
+      validateSignUpName(event.target.value, event.target.id);
+    }
+    if (event.target.id === "signup-email") {
+      validateSignUpEmail(event.target.value);
+    }
+    if (event.target.id === "signup-password") {
+      validateSignUpPassword(event.target.value);
+    }
+  };
+
+  const validateSignUpName = (signUpName, nameType) => {
+    let signUpNameValid = signUpValid;
+    let signUpNameError = "";
+
+    if (nameType === "signup-first-name") {
+      signUpNameError = signUpErrorMessages.signUpFirstNameError;
+    } else if (nameType === "signup-last-name") {
+      signUpNameError = signUpErrorMessages.signUpLastNameError;
+    }
+
+    if (signUpName.trim() === "") {
+      signUpNameValid = false;
+      signUpNameError = "Name is required";
+    } else if (signUpName.trim().length === 1) {
+      signUpNameValid = false;
+      signUpNameError = "Please enter a valid name";
+    } else {
+      signUpNameValid = true;
+      signUpNameError = "";
+    }
+
+    if (nameType === "signup-first-name") {
+      setSignUpFirstName(signUpName);
+      setSignUpValid(signUpNameValid);
+      setSignUpErrorMessages({
+        ...signUpErrorMessages,
+        signUpFirstNameError: signUpNameError,
+      });
+    } else {
+      setSignUpLastName(signUpName);
+      setSignUpValid(signUpNameValid);
+      setSignUpErrorMessages({
+        ...signUpErrorMessages,
+        signUpLastNameError: signUpNameError,
+      });
+    }
+
+    return signUpNameValid;
+  };
+
+  const validateSignUpEmail = (signUpEmail) => {
+    let signUpEmailValid = signUpValid;
+    let signUpEmailError = "";
+    const emailPattern = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+    if (!emailPattern.test(signUpEmail)) {
+      signUpEmailValid = false;
+      signUpEmailError = "Please enter a valid email";
+    } else {
+      signUpEmailValid = true;
+      signUpEmailError = "";
+    }
+
+    setSignUpEmail(signUpEmail);
+    setSignUpValid(signUpEmailValid);
+    setSignUpErrorMessages({
+      ...signUpErrorMessages,
+      signUpEmailError: signUpEmailError,
+    });
+
+    return signUpEmailValid;
+  };
+
+  const validateSignUpPassword = (signUpPassword) => {
+    let signUpPasswordValid = signUpValid;
+    let signUpPasswordError = "";
+    const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+
+    if (!passwordPattern.test(signUpPassword)) {
+      signUpPasswordValid = false;
+      signUpPasswordError =
+        "Must contain at least one number, one uppercase, one lowercase, and at least 8 characters";
+    } else {
+      signUpPasswordValid = true;
+      signUpPasswordError = "";
+    }
+
+    setSignUpPassword(signUpPassword);
+    setSignUpValid(signUpPasswordValid);
+    setSignUpErrorMessages({ ...signUpErrorMessages, signUpPasswordError });
+
+    return signUpPasswordValid;
+  };
+
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault();
+    console.log("sign up submit!");
+    if (signUpValid) {
+      setSignUpFirstName("");
+      setSignUpLastName("");
+      setSignUpEmail("");
+      setSignUpPassword("");
+      setSignUpValid(false);
+      setSignUpErrorMessages({
+        signUpFirstNameError: "",
+        signUpLastNameError: "",
+        signUpEmailError: "",
+        signUpPasswordError: "",
+      });
+      alert("successful sign up!");
+    }
+  };
 
   return (
     <div className="container-fluid vh-100 d-flex flex-column justify-content-center">
@@ -52,13 +173,13 @@ const LoginSignUp = () => {
           <Tabs
             defaultActiveKey="login"
             id="login-sign-up"
-            className="mb-3"
+            className="mb-3 bg-light"
             justify
           >
             <Tab eventKey="login" title="Login">
               <div className="row">
                 <div className="col">
-                  <Form.Group className="mb-3" controlId="email-login">
+                  <Form.Group className="mb-3" controlId="login-email">
                     <Form.Control
                       type="email"
                       placeholder="Email Address *"
@@ -71,7 +192,7 @@ const LoginSignUp = () => {
               </div>
               <div className="row">
                 <div className="col">
-                  <Form.Group className="mb-3" controlId="password-login">
+                  <Form.Group className="mb-3" controlId="login-password">
                     <Form.Control
                       type="password"
                       placeholder="Password *"
@@ -96,45 +217,102 @@ const LoginSignUp = () => {
               </div>
             </Tab>
 
+            {/* Sign Up Form */}
             <Tab eventKey="sign-up" title="Sign Up">
               <div className="row">
                 <div className="col">
-                  <Form.Group className="mb-3" controlId="first-name">
-                    <Form.Control type="text" placeholder="First Name *" />
+                  <Form.Group className="mb-3" controlId="signup-first-name">
+                    <Form.Control
+                      type="text"
+                      placeholder="First Name *"
+                      onChange={handleSignUpChange}
+                    />
+                    <Form.Text muted>
+                      {signUpErrorMessages.signUpFirstNameError}
+                    </Form.Text>
                   </Form.Group>
                 </div>
                 <div className="col">
-                  <Form.Group className="mb-3" controlId="last-name">
-                    <Form.Control type="text" placeholder="Last Name *" />
+                  <Form.Group controlId="signup-last-name">
+                    <Form.Control
+                      type="text"
+                      placeholder="Last Name *"
+                      onChange={handleSignUpChange}
+                    />
+                    <Form.Text muted>
+                      {signUpErrorMessages.signUpLastNameError}
+                    </Form.Text>
                   </Form.Group>
                 </div>
               </div>
 
-              <div className="row">
-                <div className="col">
-                  <Form.Group className="mb-3" controlId="email-signup">
-                    <Form.Control type="email" placeholder="Email Address *" />
-                  </Form.Group>
+              <Form onSubmit={handleSignUpSubmit}>
+                <div className="row">
+                  <div className="col">
+                    <Form.Group className="mb-3" controlId="signup-email">
+                      <Form.Control
+                        type="email"
+                        placeholder="Email Address *"
+                        onChange={handleSignUpChange}
+                      />
+                      <Form.Text muted>
+                        {signUpErrorMessages.signUpEmailError}
+                      </Form.Text>
+                    </Form.Group>
+                  </div>
                 </div>
-              </div>
 
-              <div className="row">
-                <div className="col">
-                  <Form.Group className="mb-3" controlId="password-signup">
-                    <Form.Control type="password" placeholder="Password *" />
-                  </Form.Group>
+                <div className="row">
+                  <div className="col">
+                    <Form.Group className="mb-3" controlId="signup-password">
+                      <Form.Control
+                        type="password"
+                        placeholder="Password *"
+                        onChange={handleSignUpChange}
+                      />
+                      <Form.Text muted>
+                        {signUpErrorMessages.signUpPasswordError}
+                      </Form.Text>
+                    </Form.Group>
+                  </div>
                 </div>
-              </div>
 
-              <div className="row mx-0">
-                <Button className="mb-3">SIGN UP</Button>
-              </div>
-
-              <div className="row mb-3">
-                <div className="col d-flex justify-content-end">
-                  <a href="#">Already have an account? Sign in</a>
+                <div className="row">
+                  <div className="col-3 d-flex justify-content-start">
+                    <Form.Label>Select Region</Form.Label>
+                  </div>
+                  <div className="col d-flex justify-content-start">
+                    <Form.Group>
+                      <Form.Check
+                        inline
+                        label="Malaysia"
+                        type="radio"
+                        id="signup-partner-malaysia"
+                        name="partner"
+                      />
+                      <Form.Check
+                        inline
+                        label="Indonesia"
+                        type="radio"
+                        id="signup-partner-indonesia"
+                        name="partner"
+                      />
+                    </Form.Group>
+                  </div>
                 </div>
-              </div>
+
+                <div className="row mx-0">
+                  <Button type="submit" className="mb-3">
+                    SIGN UP
+                  </Button>
+                </div>
+
+                <div className="row mb-3">
+                  <div className="col d-flex justify-content-end">
+                    <a href="#">Already have an account? Sign in</a>
+                  </div>
+                </div>
+              </Form>
             </Tab>
           </Tabs>
         </div>
