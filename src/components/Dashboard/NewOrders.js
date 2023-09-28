5b7b3dc59fb1349d80d78b7ca69e959372b606de
Azheem Rahman <azheem.rahman@gmail.com>
2023-09-28 14:15:15 +0800

    created new order component

---
+++ src/components/Dashboard/NewOrders.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewOrders = () => {
+  return (
+    <div>{/* New Orders assigned to NP by admin -- can accept/reject  */}</div>
+  );
+};
+
+export default NewOrders;
